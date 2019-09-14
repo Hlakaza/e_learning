962e06b25346e3a5ba2e8c9b86147834ce0a8378
Lungile FINGER <lfinger@c4r11s2.wethinkcode.co.za>
2019-09-14 08:34:47 +0200

    second day

--- HomePage/AppBodyContent/WelcomeContainer.dart
+++ HomePage/AppBodyContent/WelcomeContainer.dart
@@ -32,8 +32,8 @@ class WelcomeContainer extends StatelessWidget
       ),
 
       new CachedNetworkImage(   
-                      width: 100.0,
-                      height: 100.0,
+                      width: 150.0,
+                      height: 150.0,
                       alignment: Alignment.center,
                       imageUrl: "https://matriclive.com/new_feature/illustrations/education.gif",
                       placeholder: (context, url) =>
