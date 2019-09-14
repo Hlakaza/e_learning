70609da4c12f3e165d80e864395db9dc3620e868
Lungile FINGER <lfinger@c4r11s2.wethinkcode.co.za>
2019-09-14 08:42:01 +0200

    second day

--- HomePage/AppBodyContent/WelcomeContainer.dart
+++ HomePage/AppBodyContent/WelcomeContainer.dart
@@ -17,6 +17,7 @@ class WelcomeContainer extends StatelessWidget
       width: MediaQuery.of(context).size.width,
       child: new Row(
        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+       crossAxisAlignment: CrossAxisAlignment.spaceEvenly,
         children: <Widget> [   
           new Padding(
         padding: const EdgeInsets.all(20.0),
