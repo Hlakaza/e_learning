41b04065284e1304fb6b70bdd5334e5bcdc6819e
Lungile FINGER <lfinger@c4r11s2.wethinkcode.co.za>
2019-09-17 09:13:34 +0200

    new day

--- HomePage/AppBodyContent/English_HL/English_FAL/Stories/StoriesList.dart
+++ HomePage/AppBodyContent/English_HL/English_FAL/Stories/StoriesList.dart
@@ -63,7 +63,29 @@ class Story1 extends StatelessWidget {
               child: new ListView(
                 children: <Widget>[
 
-                
+                Html(
+  data: """
+    <!--For a much more extensive example, look at example/main.dart-->
+    <div>
+      <h1>Demo Page</h1>
+      <p>This is a fantastic nonexistent product that you should buy!</p>
+      <h2>Pricing</h2>
+      <p>Lorem ipsum <b>dolor</b> sit amet.</p>
+      <h2>The Team</h2>
+      <p>There isn't <i>really</i> a team...</p>
+      <h2>Installation</h2>
+      <p>You <u>cannot</u> install a nonexistent product!</p>
+      <!--You can pretty much put any html in here!-->
+    </div>
+  """,
+  //Optional parameters:
+  padding: EdgeInsets.all(8.0),
+  backgroundColor: Colors.white70,
+  defaultTextStyle: TextStyle(fontFamily: 'serif'),
+  linkStyle: const TextStyle(
+    color: Colors.redAccent,
+  ),
+),
 
                 ],
               ),
